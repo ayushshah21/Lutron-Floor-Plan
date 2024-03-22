e95fd3bbbdc4d8f9d30d6f947655430ee0ec9591
Amelia <khg225@lehigh.edu>
2024-03-22 13:28:01 -0400

    authentication e2e added

---
+++ floor-plan/cypress/e2e/authentication.cy.ts
@@ -0,0 +1,52 @@
+describe("Login page", () => {
+  before(() => {
+    cy.log(`Visiting http://localhost:3000`)
+    cy.visit("http://localhost:3000")
+  })
+  it("Login with Google", () => {
+    const username = Cypress.env("GOOGLE_USER")
+    const password = Cypress.env("GOOGLE_PW")
+    const loginUrl = Cypress.env("SITE_NAME")
+    const cookieName = Cypress.env("COOKIE_NAME")
+    const socialLoginOptions = {
+      username,
+      password,
+      loginUrl,
+      headless: true,
+      logs: false,
+      isPopup: true,
+      loginSelector: `a[href="${Cypress.env(
+        "SITE_NAME"
+      )}/api/auth/signin/google"]`,
+      postLoginSelector: ".unread-count",
+    }
+
+    return cy
+      .task("GoogleSocialLogin", socialLoginOptions)
+      .then(({ cookies }) => {
+        cy.clearCookies()
+
+        const cookie = cookies
+          .filter((cookie) => cookie.name === cookieName)
+          .pop()
+        if (cookie) {
+          cy.setCookie(cookie.name, cookie.value, {
+            domain: cookie.domain,
+            expiry: cookie.expires,
+            httpOnly: cookie.httpOnly,
+            path: cookie.path,
+            secure: cookie.secure,
+          })
+
+          Cypress.Cookies.defaults({
+            preserve: cookieName,
+          })
+
+          // remove the two lines below if you need to stay logged in
+          // for your remaining tests
+          cy.visit("/api/auth/signout")
+          cy.get("form").submit()
+        }
+      })
+  })
+})
--- floor-plan/src/app/Login.cy.tsx
+++ floor-plan/src/app/Login.cy.tsx
@@ -3,6 +3,7 @@ import Login from './Login'
 
 describe('<Login />', () => {
   it('renders', () => {
+    
     // see: https://on.cypress.io/mounting-react
     cy.mount(<Login />)
   })
