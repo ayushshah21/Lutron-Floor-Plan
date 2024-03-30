964788454d7f53ce9bbbaf0faeac8993f7c26c8a
Ayush Shah <aas225@lehigh.edu>
2024-03-29 21:31:20 -0400

    Added firebase storage functionality to store pdfs

--- floor-plan/src/app/editor.tsx
+++ floor-plan/src/app/editor.tsx
@@ -1,9 +1,9 @@
 "use client";
 import {
-    signInWithPopup,
-    signOut,
-    onAuthStateChanged,
-    User,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged,
+  User,
 } from "firebase/auth";
 import { useState, useEffect, useRef } from "react";
 import "./editor.css";
@@ -11,72 +11,94 @@ import { auth, googleProvider } from "../../firebase";
 
 // Needed for react-pdf to work
 import { Document, Page, pdfjs } from "react-pdf";
+import { useUploadPdf } from "./useUploadPdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-
 interface HomeProps {
-    user: User | null;
-    setUser: (user: User | null) => void;
+  user: User | null;
+  setUser: (user: User | null) => void;
 }
 
 export default function Editor({ user, setUser }: HomeProps) {
-    // Store state of pdfFile
-    const [pdfFile, setPdfFile] = useState<File | null>(null);
+  // Store state of pdfFile
+  const [pdfFile, setPdfFile] = useState<File | null>(null);
+  const { uploadPdf, uploading, error } = useUploadPdf(); 
 
-    // Sign out 
-    const signOutWithGoogle = async () => {
-        try {
-            await signOut(auth);
-            setUser(null);
-        } catch (err) {
-            console.error(err);
-        }
-    };
 
-    // Checks file type and if it is a pdf
-    const handleFileChange = (event: any) => {
-        const file = event.target.files[0];
-        if (file && file.type === "application/pdf") {
-            setPdfFile(file);
-        } else {
-            alert("Please select a valid PDF file.");
-        }
-    };
+  // Sign out
+  const signOutWithGoogle = async () => {
+    try {
+      await signOut(auth);
+      setUser(null);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-    return (
-        <main className="main">
+  const handleUpload = async () => {
+    await uploadPdf(pdfFile);
+    if (error) {
+      alert(error);
+    } else {
+      alert("PDF uploaded successfully!");
+    }
+  };
 
-            <nav className="navbar">
-                <img
-                    className="lutronLogo"
-                    src="https://umslogin.lutron.com/Content/Dynamic/Default/Images/logo-lutron-blue.svg"
-                    alt="Lutron-logo"
-                />
-                <h1 className="navbarBrand">Test Floor Plan #1 </h1>
-                <ul className="navbarNav">
-                    <li className="navbarItem">
-                        <button className="button" onClick={() => signOutWithGoogle()}>Logout</button>
-                    </li>
-                </ul>
-            </nav>
+  // Checks file type and if it is a pdf
+  const handleFileChange = (event: any) => {
+    const file = event.target.files[0];
+    if (file && file.type === "application/pdf") {
+      setPdfFile(file);
+    } else {
+      alert("Please select a valid PDF file.");
+    }
+  };
 
-            {/* Put side tool bar over here, and make it appear to the left of the canvas box */}
-            <div className="sideToolBar">
-                <form>
-                    <input type="file" onChange={handleFileChange} accept="application/pdf" />
-                </form>
-                <button>Export</button>
-            </div>
+  return (
+    <main className="main">
+      <nav className="navbar">
+        <img
+          className="lutronLogo"
+          src="https://umslogin.lutron.com/Content/Dynamic/Default/Images/logo-lutron-blue.svg"
+          alt="Lutron-logo"
+        />
+        <h1 className="navbarBrand">Test Floor Plan #1 </h1>
+        <ul className="navbarNav">
+          <li className="navbarItem">
+            <button className="button" onClick={() => signOutWithGoogle()}>
+              Logout
+            </button>
+          </li>
+        </ul>
+      </nav>
 
-            <div className="canvasBox">
-                {/* Displays pdf*/}
-                {/* Add feature to zoom in and zoom out of pdf */}
-                {pdfFile && (
-                    <Document file={URL.createObjectURL(pdfFile)}>
-                        <Page pageNumber={1} renderAnnotationLayer={false} renderTextLayer={false} />
-                    </Document>
-                )}
-            </div>
-        </main>
-    );
+      {/* Put side tool bar over here, and make it appear to the left of the canvas box */}
+      <div className="sideToolBar">
+        <form>
+          <input
+            type="file"
+            onChange={handleFileChange}
+            accept="application/pdf"
+          />
+        </form>
+        <button onClick={handleUpload} disabled={uploading}>
+          {uploading ? "Uploading..." : "Export"}
+        </button>
+      </div>
+
+      <div className="canvasBox">
+        {/* Displays pdf*/}
+        {/* Add feature to zoom in and zoom out of pdf */}
+        {pdfFile && (
+          <Document file={URL.createObjectURL(pdfFile)}>
+            <Page
+              pageNumber={1}
+              renderAnnotationLayer={false}
+              renderTextLayer={false}
+            />
+          </Document>
+        )}
+      </div>
+    </main>
+  );
 }
