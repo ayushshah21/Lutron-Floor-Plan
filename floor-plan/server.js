d7a51093e7a00989f1893268e8038bd7a2ddf05e
Ivan Zhang <ivanzhang0111@gmail.com>
2024-09-15 21:40:51 -0400

    Installed socket.io and express.js and created a server template

---
+++ floor-plan/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+  const httpServer = createServer(server);
+  const io = new Server(httpServer);
+
+  io.on('connection', (socket) => {
+    console.log('Client connected');
+
+    socket.on('disconnect', () => {
+      console.log('Client disconnected');
+    });
+
+    // Handle custom events here
+  });
+
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  httpServer.listen(3000, (err) => {
+    if (err) throw err;
+    console.log('> Ready on http://localhost:3000');
+  });
+});
