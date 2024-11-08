5e47091be755ae9642a4592aca63c115546a0409
Ivan Zhang <ivanzhang0111@gmail.com>
2024-11-08 16:39:38 -0500

    Deleted unnecessary files, and fixed CSS bug for login page

--- floor-plan/src/app/login/page.tsx
+++ floor-plan/src/app/login/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import { signInWithPopup } from "firebase/auth";
+import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { auth, googleProvider } from "../../../firebase";
 import Image from "next/image";
-import './login.css';
+import styles from './login.module.css';
 
 export default function Login() {
 	const router = useRouter();
@@ -18,29 +19,42 @@ export default function Login() {
 		}
 	};
 
+	// Removes the white border around the login page
+	useEffect(() => {
+		document.body.style.margin = "0";
+		document.body.style.padding = "0";
+		document.body.style.boxSizing = "border-box";
+		document.body.style.background = "radial-gradient(circle, #1E75BB 0%, #00457C 100%)";
+	
+		return () => {
+		  document.body.style.margin = "";
+		  document.body.style.padding = "";
+		  document.body.style.boxSizing = "";
+		  document.body.style.background = "";
+		};
+	  }, []);
+
 	return (
-		<main className="main">
-			<div className="loginBox" id="loginBox">
+		<div className={styles.loginWrapper}>
+			<div className={styles.loginBox} id="loginBox">
 				<Image
 					src="https://umslogin.lutron.com/Content/Dynamic/Default/Images/logo-lutron-blue.svg"
 					alt="Lutron-logo"
 					width={150}
 					height={25}
-					className="lutronLogo"
+					className={styles.lutronLogo}
 				/>
 				<h3>Sign In</h3>
-				<p>with your gmail account</p>
-				<button className="button" onClick={signInWithGoogle} id="loginButton">
+				<p>with your Gmail account</p>
+				<button className={styles.button} onClick={signInWithGoogle} id="loginButton">
 					Google Login
 				</button>
-				<div className="footer">
-					<div>
-						<a href="http://www.lutron.com/myLutronPrivacyNotice">Privacy Notice</a> / 
-						<a href="http://www.lutron.com/myLutronCookieNotice">Cookie Notice</a> / 
-						<a href="mailto:mylutronsupport@lutron.com">Support</a>
-					</div>
+				<div className={styles.footer}>
+					<a href="http://www.lutron.com/myLutronPrivacyNotice">Privacy Notice</a> /
+					<a href="http://www.lutron.com/myLutronCookieNotice">Cookie Notice</a> /
+					<a href="mailto:mylutronsupport@lutron.com">Support</a>
 				</div>
 			</div>
-		</main>
+		</div>
 	);
 }
--- floor-plan/src/app/page.tsx
+++ floor-plan/src/app/page.tsx
@@ -1,17 +1,10 @@
 "use client";
-import Link from "next/link";
-import { Inter } from "next/font/google";
-import styles from "./page.module.css"; // Ensure you have some basic styles
-import { User } from "firebase/auth";
 import { auth } from "../../firebase";
 import { useRouter } from "next/navigation";
 import { useEffect } from 'react';
 
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Page() {
   const router = useRouter();
-  const currentUser = auth.currentUser;
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(user => {
@@ -25,5 +18,5 @@ export default function Page() {
     return () => unsubscribe();
   }, [router]);
 
-  return null; //We should probably create a nice landing page sometime soon to put here
+  return null; 
 }
